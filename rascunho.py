521944ac64b74a1af4a0979250a2936933b8f7d2
Manteiga <rafael.manteiga@hotmail.com>
2022-05-06 04:23:41 -0300

    Update rascunho.py

--- rascunho.py
+++ rascunho.py
@@ -116,3 +116,20 @@ p3 = pessoa('Diego',38)
 pessoas = [p1,p2,p3]
 print(sorted(pessoas,key = pelonome))
 print(sorted(pessoas,key = pelaidade))
+
+def pai(num):
+    def filho1():
+        print("Sou o filho1")
+    def filho2():
+        print("Sou o filho2")
+    try:
+        assert num == 20
+        return filho1
+    except AssertionError:
+        return filho2
+
+f1 = pai(10)
+f2 = pai(20)
+
+f1()
+f2()
