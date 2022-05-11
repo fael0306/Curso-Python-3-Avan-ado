604fc33758f9d86480c8c58970ecbd523a36503d
Manteiga <rafael.manteiga@hotmail.com>
2022-05-11 12:40:13 -0300

    Update grupos.py

--- grupos.py
+++ grupos.py
@@ -1,10 +1,8 @@
-fotosagatha = {"Rafael","Rafaela","Sandra","Adriano","Alícia","Diego","Gabriel","Gui","Josi1","Josi2",
-                "Júnior","Mãe","Pai","Roland","Lourdes","Lourdes","Tio Rafael"}
+fotosagatha = {"Rafael","Rafaela","Sandra","Adriano","Alícia","Diego","Gabriel","Gui","Josi1","Josi2","Júnior","Mãe","Pai","Roland","Lourdes","Lourdes","Tio Rafael"}
 mabame = {"Rafael","Adriano","Carla","Gabriel","Mãe","Nilza","Pai"}
 maedeutsch = {"Rafael","Adriano","Diego","Gabriel","Mãe","Pai"}
 trioparadadura = {"Rafael","Mãe","Pai"}
-fammanteigabrde = {"Rafael","Adriano","Diego","Gabriel","Josi2","Júnior","Nilza","Rafaela","Lourdes",
-                    "Tio Rafael","João Gabriel","Mãe"}
+fammanteigabrde = {"Rafael","Adriano","Diego","Gabriel","Josi2","Júnior","Nilza","Rafaela","Lourdes","Tio Rafael","João Gabriel","Mãe"}
 familiatrololo = {"Rafael","Gabriel","Mãe","Pai"}
 
 uniao = (fotosagatha | mabame | maedeutsch | trioparadadura | fammanteigabrde | familiatrololo)
