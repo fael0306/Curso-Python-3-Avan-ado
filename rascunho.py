8f64118a0cf731966dbe723c336c64e52fc53032
Manteiga <rafael.manteiga@hotmail.com>
2022-05-12 04:55:24 -0300

    Update rascunho.py

--- rascunho.py
+++ rascunho.py
@@ -772,3 +772,27 @@ import itertools
 for x in itertools.islice(c,10,20):
     print(x)
 
+class A:
+    def fazer_algo(self):
+        pass
+    def outro_metodo(self):
+        pass
+    def algum_metodo(self,nome):
+        print(nome)
+
+class B:
+    def __init__(self):
+        self.a = A()
+    def fazer_algo(self):
+        # Delega para self.a
+        return self.a.fazer_algo()
+    def outro_metodo(self):
+        # Delega para self.a
+        return self.a.outro_metodo()
+    def __getattr__(self, nome):
+        return getattr(self.a,nome)
+
+b = B()
+b.fazer_algo()
+b.algum_metodo("Python")
+
