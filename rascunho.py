ca9feed0f3e549ef07013afe36c72623a2d8b87b
Manteiga <rafael.manteiga@hotmail.com>
2022-05-13 02:43:42 -0300

    Update rascunho.py

--- rascunho.py
+++ rascunho.py
@@ -916,3 +916,40 @@ print(jsonstr)
 print(type(jsonstr))
 print(data)
 print(type(data))
+
+print(1+2)
+print((1).__add__(2))
+
+class MeuInt(int):
+    def __add__(self, num):
+        return 0
+
+a = MeuInt(1)
+print(a+2)
+
+print(dir(list))
+help(list.append)
+
+# Sobrescrevendo um método usando OO
+class MinhaLista(list):
+    def append(self, *args):
+        self.extend(args)
+
+l = MinhaLista()
+l.append(1)
+l.append(2,3,4,5,6,7)
+print(l)
+
+class MyList(list):
+    def sort(self):
+        return "Opa, você quer ordenar"
+
+lista = [10,5,20]
+lista.sort()
+print(lista)
+
+lista = MyList()
+
+print(lista.sort()) # Retorna diferente pois está sobrescrito
+
+print(help(list.sort))
